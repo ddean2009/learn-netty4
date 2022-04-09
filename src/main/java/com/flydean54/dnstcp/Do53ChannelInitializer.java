2c51240da3b77dfd9377842163fffe95d3116873
wayne <wayne@ouba.local>
2022-04-09 20:26:43 +0800

    netty系列之:在netty中使用TCP协议请求DNS服务器

---
+++ src/main/java/com/flydean54/dnstcp/Do53ChannelInitializer.java
@@ -0,0 +1,17 @@
+package com.flydean54.dnstcp;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.dns.TcpDnsQueryEncoder;
+import io.netty.handler.codec.dns.TcpDnsResponseDecoder;
+
+class Do53ChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) {
+        ChannelPipeline p = ch.pipeline();
+        p.addLast(new TcpDnsQueryEncoder())
+                .addLast(new TcpDnsResponseDecoder())
+                .addLast(new Do53ChannelInboundHandler());
+    }
+}
---
+++ src/main/java/com/flydean54/dnstcp/Do53TcpClient.java
@@ -0,0 +1,49 @@
+
+package com.flydean54.dnstcp;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.dns.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public final class Do53TcpClient {
+
+    public void startDnsClient(String dnsServer,int dnsPort, String queryDomain) throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new Do53ChannelInitializer());
+
+            final Channel ch = b.connect(dnsServer, dnsPort).sync().channel();
+
+            int randomID = (int) (System.currentTimeMillis() / 1000);
+            DnsQuery query = new DefaultDnsQuery(randomID, DnsOpCode.QUERY)
+                    .setRecord(DnsSection.QUESTION, new DefaultDnsQuestion(queryDomain, DnsRecordType.A));
+            ch.writeAndFlush(query).sync();
+            boolean result = ch.closeFuture().await(10, TimeUnit.SECONDS);
+            if (!result) {
+                log.error("DNS查询失败");
+                ch.close().sync();
+            }
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Do53TcpClient client = new Do53TcpClient();
+        final String dnsServer = "223.5.5.5";
+        final int dnsPort = 53;
+        final String queryDomain ="www.flydean.com";
+        client.startDnsClient(dnsServer,dnsPort,queryDomain);
+    }
+
+}
